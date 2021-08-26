b2977ef1a1286a0672ba953e5e24e982f3db135d
Paul van Buuren <paul@wbvb.nl>
2021-08-26 13:36:50 +0200

    script corrected

--- corrections.sh
+++ corrections.sh
@@ -1,24 +1,18 @@
 
 # sh 'corrections.sh' &>/dev/null
 
-echo 'Change name';
+echo 'events-manager.php: Change name';
 sed -i '.bak' "s/Plugin Name: Events Manager/Plugin Name: Events Manager (ICTU WP corrections)/g" 'events-manager.php';
 
-echo 'Change signature';
+echo 'events-manager.php: Change signature';
 sed -i '.bak' "s/Author: Marcus Sykes/Author: Marcus Sykes (Paul van Buuren)/g" 'events-manager.php';
 
-
-
-
-
-
-echo 'Change hook name';
+echo 'events-manager.php: Change hook name';
 sed -i '.bak' "s/add_action( 'events_manager_loaded'/add_action( 'template_redirect'/g" 'events-manager.php';
 
 
-echo 'Change replace method';
-sed -i '.bak' "s|$content = str_replace('CONTENTS',$content,$page_content)|$content = preg_replace( '/CONTENTS/', $page_content, $content )|g" 'em-events.php';
-
+echo 'em-events.php: Change replace method';
+sed -i '.bak' "s|\$content = str_replace('CONTENTS',\$content,\$page_content)|\$content = preg_replace( '/CONTENTS/', \$page_content, \$content )|g" 'em-events.php';
 
 
 
