fd9cafda1d3723e3f83fd5b8589597cf1c07a5d3
Paul van Buuren <paul@wbvb.nl>
2023-01-24 14:30:41 +0100

    removed title attributes

--- corrections.sh
+++ corrections.sh
@@ -54,27 +54,54 @@ echo 'em-bookings-table.php: meer dan 100 boekingen op 1 pagina';
 cd 'classes';
 pwd;
 sed -i '.bak' "s|<option value=\"100\">100<\/option>|<option  value=\"100\">100<\/option><option value=\"250\">250<\/option><option value=\"500\">500<\/option><option value=\"2000\">2000<\/option><option value=\"5000\">5000<\/option><option value=\"10000\">10000<\/option>|g" 'em-bookings-table.php';
-# cd ..;
 
-
-cd '/Users/paul/shared-paul-files/Webs/git-repos/ictuwp-plugins-eventsmanager-overrides/ictuwp-plugins-eventsmanager-overrides/admin/bookings/';
-
-sed -i '.bak' "s| title=\"List View\"||g" 'em-cancelled.php';
-
-
-cd '/Users/paul/shared-paul-files/Webs/git-repos/ictuwp-plugins-eventsmanager-overrides/ictuwp-plugins-eventsmanager-overrides/templates/forms/event/';
-pwd;
-ls -l;
-
-sed -i '.bak' "s| title=\"<\"?php echo esc_attr(\"{$EM_Location->location_latitude},{$EM_Location->location_longitude}\"); ?>\"||g" 'location.php';
-
-
-
-#/Users/paul/shared-paul-files/Webs/git-repos/ictuwp-plugins-eventsmanager-overrides/ictuwp-plugins-eventsmanager-overrides/
-#admin/bookings/
-
-
+#remove any title attributes in all .php files
 cd '/Users/paul/shared-paul-files/Webs/git-repos/ictuwp-plugins-eventsmanager-overrides/ictuwp-plugins-eventsmanager-overrides/';
+pwd;
+echo 'remove title attributes';
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"List View\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"<\"?php echo esc_attr(\"{$EM_Location->location_latitude},{$EM_Location->location_longitude}\"); ?>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php _e ( \'Duplicate this event\', \'events-manager\'); ?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\' . __( \'My Bookings\', \'events-manager\') . \'\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php esc_attr_e(\'Add a start or end date (or both) to impose time constraints on ticket availability. Leave either blank for no upper\/lower limit.\',\'events-manager\'); ?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php esc_attr_e(\'If checked every booking must select one or the minimum number of this ticket.\',\'events-manager\'); \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php esc_attr_e(\'Enter a ticket name.\',\'events-manager\'); \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\#_CATEGORYNAME\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php echo esc_attr ( sprintf(__(\'Duplicate \%s\',\'events-manager\'), __(\'Event\',\'events-manager\')) ); ?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php esc_attr_e(\'Enter a maximum number of spaces (required).\',\'events-manager\'); \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php __(\'Click to toggle\', \'events-manager\'); \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"Remove\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php esc_attr_e(\'Leave either blank for no upper\/lower limit.\',\'events-manager\'); \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php esc_html_e(\'Password Lost and Found\', \'events-manager\') \?>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php echo esc_attr(\"{\$EM_Location->location_latitude},{\$EM_Location->location_longitude}\"); \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\' . esc_attr( sprintf( __( \"View all posts in \%s\", \'events-manager\'), \$category-\>name ) ) . \'\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php echo \$cap_help; \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"Excerpt View\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\'.esc_attr(str_replace(\'_\', \' \', \$EM_Event-\>event_timezone)).\'\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\'.sprintf(__(\'Duplicate \%s\',\'events-manager\'), __(\'Event\',\'events-manager\')).\'\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\'.esc_attr(\$text).\'\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\'.esc_attr(\$this-\>location_name).\'\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php esc_attr_e(\'Booking Actions\', \'events-manager\'); \?\>\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\'.esc_attr(\$this-\>event_name).\'\"||g" {} +
+find . -type f -name '*.php' -exec sed -i '' "s| title=\"\<\?php echo esc_attr(\$cell_data\[\'link_title\'\]); \?\>\"||g" {} +
+#find . -type f -name '*.php' -exec sed -i '' "s|REPLACETHIS||g" {} +
+#find . -type f -name '*.php' -exec sed -i '' "s|REPLACETHIS||g" {} +
+#find . -type f -name '*.php' -exec sed -i '' "s|REPLACETHIS||g" {} +
+#find . -type f -name '*.php' -exec sed -i '' "s|REPLACETHIS||g" {} +
 
+# updates stuk maken
+echo 'em-admin.php: break update checks';
+cd '/Users/paul/shared-paul-files/Webs/git-repos/ictuwp-plugins-eventsmanager-overrides/ictuwp-plugins-eventsmanager-overrides/admin/'
+sed -i '.bak' "s|add_filter(\'pre_set_site_transient_update_plugins|\/\/ add_filter(\'pre_set_site_transient_update_plugins|g" 'em-admin.php';
+
+
+echo 'events-manager.php: Assign stoopid high version number';
+cd '/Users/paul/shared-paul-files/Webs/git-repos/ictuwp-plugins-eventsmanager-overrides/ictuwp-plugins-eventsmanager-overrides/';
+sed -i '.bak' "s|EM_VERSION', \'6.|EM_VERSION', \'999.|g" 'events-manager.php';
+sed -i '.bak' "s|Version: 6.|Version: 999.|g" 'events-manager.php';
+
+# remove .bak files
 find . -name '*.bak' -type f -delete
 
+echo 'Copy to local dev environment';
+sh '/Users/paul/shellscripts/gebruikercentraal/ictuwp-plugins-eventsmanager-overrides.sh'
